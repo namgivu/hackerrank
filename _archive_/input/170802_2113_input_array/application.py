ac411f6a8d01969de326eb1c2a49a4187f2529ce
namgivu <namgivu@gmail.com>
2017-08-02 21:15:10 +0700

    input sample

---
+++ _archive_/input/170802_2113_input_array/application.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+
+"""https://www.hackerrank.com/challenges/equal-stacks"""
+
+import sys
+
+n1,n2,n3 = raw_input().strip().split(' ')
+n1,n2,n3 = [int(n1),int(n2),int(n3)]
+h1 = map(int,raw_input().strip().split(' '))
+h2 = map(int,raw_input().strip().split(' '))
+h3 = map(int,raw_input().strip().split(' '))
