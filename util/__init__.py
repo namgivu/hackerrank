05ad5792e73e0435ee1f2786a8fd591ff6c9d5b8
namgivu <namgivu@gmail.com>
2017-08-03 12:11:52 +0700

    flush output file util

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,17 @@
-def setupPycharmDebug(inputFile, outputFile):
+INPUT_FILE='input.txt'
+OUTPUT_FILE='output.txt'
+
+
+def flushOutput():
+  """
+  you may need to re-sync the selected output file in PyCharm - as NN setting, ctrl-shift-` will do
+  write/flush immediately ref. https://stackoverflow.com/a/230774/248616
+  """
+  import sys
+  sys.stdout.flush()
+
+
+def setupPycharmDebug(inputFile=INPUT_FILE, outputFile=OUTPUT_FILE):
   """
   redirect input stream to input file for PyCharm debug ref. https://stackoverflow.com/a/39482389/248616
   redirect output stream to output file for PyCharm debug ref https://stackoverflow.com/a/4675744/248616
@@ -12,4 +25,4 @@ def setupPycharmDebug(inputFile, outputFile):
   sys.stdin = StringIO.StringIO(lines)
 
   #output
-  sys.stdout = open(outputFile, 'w')
+  sys.stdout = open(outputFile, mode='w', buffering=0)
