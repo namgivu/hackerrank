aea7ae834e06bf3497ffc2a0d7dea2c8566b45bf
namgivu <namgivu@gmail.com>
2017-08-03 11:21:59 +0700

    pychar debug util

--- _TEMPLATE_/yymmdd_hhMM_debug/application.py
+++ _TEMPLATE_/yymmdd_hhMM_debug/application.py
@@ -5,7 +5,7 @@ https://www.hackerrank.com/challenges/YOUR-TOPIC
 """
 
 #turn this on when debug to redirect stdin to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
-# from util import * ; setupPycharmDebug('input.txt')
+# from util import * ; setupPycharmDebug('input.txt', 'output.txt')
 
 import sys
 N = int(sys.stdin.readline())
@@ -14,3 +14,5 @@ a = []
 a = map(int, sys.stdin.readline().split(' '))
 
 print N, a
+sys.stdout.flush()  # write/flush immediately ref. https://stackoverflow.com/a/230774/248616
+
--- util/__init__.py
+++ util/__init__.py
@@ -1,6 +1,15 @@
-def setupPycharmDebug(inputFile):
-  """redirect input stream to input file for PyCharm debug"""
+def setupPycharmDebug(inputFile, outputFile):
+  """
+  redirect input stream to input file for PyCharm debug ref. https://stackoverflow.com/a/39482389/248616
+  redirect output stream to output file for PyCharm debug ref https://stackoverflow.com/a/4675744/248616
+  """
+
   import sys
   import StringIO
-  input = "".join(open(inputFile, "r").readlines())
-  sys.stdin = StringIO.StringIO(input)
+
+  #input
+  lines = "".join(open(inputFile, "r").readlines())
+  sys.stdin = StringIO.StringIO(lines)
+
+  #output
+  sys.stdout = open(outputFile, 'w')
