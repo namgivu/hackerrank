d5753dc82d27524616019edd7ff903aaa01e5414
namgivu <namgivu@gmail.com>
2017-08-03 12:14:36 +0700

    template

--- _TEMPLATE_/yymmdd_hhMM_debug/application.py
+++ _TEMPLATE_/yymmdd_hhMM_debug/application.py
@@ -4,8 +4,10 @@
 https://www.hackerrank.com/challenges/YOUR-TOPIC
 """
 
+from util import *
+
 #turn this on when debug to redirect stdin to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
-# from util import * ; setupPycharmDebug()
+#setupPycharmDebug()
 
 import sys
 N = int(sys.stdin.readline())
@@ -13,6 +15,7 @@ N = int(sys.stdin.readline())
 a = []
 a = map(int, sys.stdin.readline().split(' '))
 
-print N, a
-sys.stdout.flush()  # write/flush immediately ref. https://stackoverflow.com/a/230774/248616
+print(N)
+flushOutput()
+print(a)
 
