9c11d2397d07482e092486cc957eefa0de8d3e61
namgivu <namgivu@gmail.com>
2017-08-07 10:32:19 +0700

    moving

--- _hackerrank_/_TEMPLATE_/yymmdd_hhMM_debug/application.py
+++ _hackerrank_/_TEMPLATE_/yymmdd_hhMM_debug/application.py
@@ -5,6 +5,7 @@ https://www.hackerrank.com/challenges/YOUR-TOPIC
 """
 
 DEBUG = False #turn this TRUE/False to turn ON/off for PyCharm debug; MUST turn off when submitted
+DEBUG = True #turn this TRUE/False to turn ON/off for PyCharm debug; MUST turn off when submitted
 
 #turn this on when debug to redirect stdin to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
 if DEBUG: from util import * ; setupPycharmDebug()
