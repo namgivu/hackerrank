4cbde9242d92610f075be3a97ce98b0971906792
namgivu <namgivu@gmail.com>
2017-08-10 12:41:02 +0700

    review import

--- _hackerrank_/_TEMPLATE_/yymmdd_hhMM_debug/application.py
+++ _hackerrank_/_TEMPLATE_/yymmdd_hhMM_debug/application.py
@@ -8,7 +8,7 @@ DEBUG = False #turn this TRUE/False to turn ON/off for PyCharm debug; MUST turn 
 DEBUG = True #turn this TRUE/False to turn ON/off for PyCharm debug; MUST turn off when submitted
 
 #turn this on when debug to redirect stdin to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
-if DEBUG: from util import * ; setupPycharmDebug()
+if DEBUG: from _hackerrank_.util import * ; setupPycharmDebug()
 
 import sys
 N = int(sys.stdin.readline().strip())
@@ -17,6 +17,6 @@ a = []
 a = map(int, sys.stdin.readline().strip().split(' '))
 
 print(N)
-if DEBUG: from util import *; flushOutput()
+if DEBUG: from _hackerrank_.util import *; flushOutput()
 print(a)
 
