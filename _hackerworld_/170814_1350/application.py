3d6a66ec1db7f6b2f84463dc84c755ac35f7d334
namgivu <namgivu@gmail.com>
2017-08-14 13:56:45 +0700

    hackerworld 0th

---
+++ _hackerworld_/170814_1350/application.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+"""
+https://www.hackerrank.com/challenges/YOUR-TOPIC
+"""
+
+REDIRECT_STDIO_2_FILE = False
+REDIRECT_STDIO_2_FILE = True
+
+#turn this on when debug to redirect stdin/stdout to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import * ; redirectStdio2File()
+
+import sys
+n = int(sys.stdin.readline().strip())
+s = sys.stdin.readline().strip()
+
+print(n*2)
+print(s)
+
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import *; flushOutput()
+
