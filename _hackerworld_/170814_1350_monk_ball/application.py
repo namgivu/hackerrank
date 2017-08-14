e962c3e4b1565b8842ce33c6cf5398570598ca67
namgivu <namgivu@gmail.com>
2017-08-14 18:20:03 +0700

    monk ball

---
+++ _hackerworld_/170814_1350_monk_ball/application.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2.7
+
+"""
+https://www.hackerearth.com/YOUR-TOPIC
+"""
+
+REDIRECT_STDIO_2_FILE = False
+REDIRECT_STDIO_2_FILE = True
+
+#turn this on when debug to redirect stdin/stdout to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import * ; redirectStdio2File()
+
+import sys
+K = int(sys.stdin.readline().strip())
+for _ in xrange(K):
+  #input
+  N = int(sys.stdin.readline().strip())
+  a=[]
+  for __ in xrange(N):
+    ball = int(sys.stdin.readline().strip())
+    a.append(ball)
+
+  #count bit
+  countBit=dict()
+  for ball in a:
+    b = bin(ball)
+    n = len(b)
+    for i in xrange(n):
+      if b[i]=='1':
+        j = n-1 - i
+        if j in countBit:
+          countBit[j] += 1
+        else:
+          countBit[j] = 1
+
+  # print countBit
+
+  #find max
+  countMax = -sys.maxint
+  iMax = -1
+  for i,count in countBit.items():
+    if count>countMax:
+      countMax = count
+      iMax = i
+    elif count==countMax:
+      if i<iMax:
+        iMax = i
+
+  #output
+  print(iMax)
+
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import *; flushOutput()
+
