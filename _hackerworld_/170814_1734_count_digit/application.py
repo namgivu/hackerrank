7b154d9c9c1454cc3d6e3e9472c325b687298cce
namgivu <namgivu@gmail.com>
2017-08-14 17:48:26 +0700

    count digit

---
+++ _hackerworld_/170814_1734_count_digit/application.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+"""
+https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/tutorial/
+"""
+
+REDIRECT_STDIO_2_FILE = False
+REDIRECT_STDIO_2_FILE = True
+
+#turn this on when debug to redirect stdin/stdout to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import * ; redirectStdio2File()
+
+
+import sys
+S = sys.stdin.readline().strip()
+
+c='0'
+d=dict()
+for c in xrange(10):
+  d[str(c)]=0
+
+for c in S:
+  d[c]+=1
+
+for c in xrange(10):
+  c = str(c)
+  print('%s %s' % (c, d[c]))
+
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import *; flushOutput()
+
