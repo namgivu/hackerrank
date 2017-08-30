9ed1dc2b2656d76090330d53a4935489f2f84ec4
namgivu <namgivu@gmail.com>
2017-08-30 21:22:15 +0700

    max value of a stack

---
+++ _hackerrank_/170830_2010_max_elem/application.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2.7
+
+"""
+https://www.hackerrank.com/challenges/YOUR-TOPIC
+"""
+
+REDIRECT_STDIO_2_FILE = False
+REDIRECT_STDIO_2_FILE = True
+
+#turn this on when debug to redirect stdin/stdout to file in PyCharm ref. https://stackoverflow.com/a/39482389/248616
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import * ; redirectStdio2File()
+
+
+import sys
+N = int(sys.stdin.readline().strip())
+
+st = []
+maxVal = -sys.maxint
+for _ in xrange(N):
+  line = map(int, sys.stdin.readline().strip().split(' '))
+  cmd = line[0]
+  if cmd==1: val = line[1]
+
+  if False: pass
+
+  elif cmd==1: #push
+    st.append(val)
+    if val > maxVal:
+      maxVal = val
+
+  elif cmd==2: #pop
+    val = st.pop()
+    if val == maxVal:
+      maxVal = max(st) if len(st)>0 else -sys.maxint
+
+  elif cmd==3: #print max
+    print(maxVal)
+
+
+if REDIRECT_STDIO_2_FILE: from _hackerrank_.util import *; flushOutput()
---
+++ _hackerrank_/170830_2010_max_elem/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
+
+#python path
+h="$SCRIPT_HOME/../.." ; h=$(cd "$h" && pwd) ; HACKERRANK_HOME=$h
+export PYTHONPATH="$HACKERRANK_HOME:$PYTHONPATH"
+
+#do run
+python application.py < input.txt > output.txt
