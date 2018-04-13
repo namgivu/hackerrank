d238c3b2c10802bed2e6d26502add421fa69aa8f
namgivu <namgivu@gmail.com>
2018-04-14 02:51:41 +0700

    handle assert error

--- misc/catch_assert_exception.py
+++ misc/catch_assert_exception.py
@@ -1,6 +1,12 @@
 try:
-    assert 1==2, Exception('Some error 22')
+    assert 1==2, 'Some error 22'
     print(1)
-except RuntimeError as e:
+except Exception as e:
+    from pprint import pprint
+    pprint(e)
     print(e)
+    print(type(e))
+    print(str(e))
     print(22)
+
+    #can cast Exception to falcon.HTTPBadRequest() here
