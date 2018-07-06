25820b69f4da27eeb94dcf5737bfdf4d5aaa7372
namgivu <namgivu@gmail.com>
2018-07-06 19:12:57 +0700

    demo sched.sheduler()

--- misc/queue_method_call.py
+++ misc/queue_method_call.py
@@ -1,17 +1,18 @@
-def run(self):
-    """
-    demo of using sched to queue method calls and get them executed manually
-    ref. https://bip.weizmann.ac.il/course/python/PyMOTW/PyMOTW/docs/sched/index.html
-    ref. https://docs.python.org/2/library/sched.html
-    """
+"""
+demo of using sched to queue method calls and get them executed manually
+ref. https://bip.weizmann.ac.il/course/python/PyMOTW/PyMOTW/docs/sched/index.html
+ref. https://docs.python.org/2/library/sched.html
+"""
 
-    def f1(): return print(1)
 
-    def f2(n): return print(n)
+def run1():
+    def f1(): print(1)
 
-    def f3(): return print(333)
+    def f2(n): print(n)
 
-    def f4(n, m): return print(n, m)
+    def f3(): print(333)
+
+    def f4(n, m): print(n, m)
 
     import sched
     s = sched.scheduler()
@@ -21,4 +22,16 @@ def run(self):
     s.enter(0, 0, f4, (4, 55))
     s.run()
 
-run()
+
+def run2():
+    def f5(*args, **kwargs):
+        print(args)
+        print(kwargs)
+
+    import sched
+    s = sched.scheduler()
+    f5(1,22, a=333, b=4444)
+    s.enter(0, 0, action=f5, argument=(1,22), kwargs=dict(a=333, b=4444) )
+    s.run()
+
+run2()
