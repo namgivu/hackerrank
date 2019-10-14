5f84f023a103fd0961510a564f060ba8c0b5cea0
namgivu <nam.gi.vu@gigacover.com>
2019-10-14 19:32:00 +0700

    clear

--- misc/array_append.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-a=[1,22]; print(a)
-
-a.append([333]); print(a)
-a.append(4444); print(a)
-
-a.extend([333]); print(a)
--- misc/global_var_vs_method/global_var_vs_method.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import some_module
-some_module.method1()
-some_module.method2()
-some_module.method3()
