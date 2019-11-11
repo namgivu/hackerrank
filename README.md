d07aacc51da47f7280bd7b10d833b374a872e85c
namgivu <namgivu@gmail.com>
2019-11-11 14:07:26 +0700

    demo 0th

---
+++ README.md
@@ -0,0 +1,4 @@
+run code
+```bash
+source .env; python main.py
+```
---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+env_key='SOME_VAR1';    v=os.environ.get(env_key);      print(v)
+env_key='SOME_VAR22';   v=os.environ.get(env_key);      print(v)
