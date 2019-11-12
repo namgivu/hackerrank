27b4d7b31ae6246cf22b9e87cbd0ef295ca6093f
namgivu <namgivu@gmail.com>
2019-11-12 10:48:39 +0700

    review readme

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+"""
+**goal**
+demo reading .env via os.environ dict
+
+**run it**
+               python src/main.py  # no .env var be loaded
+SOME_VAR1=1222 python src/main.py  # only SOME_VAR1 available
+source .env;   python src/main.py  # all vars defined in .env available
+pipenv run     python src/main.py  # pipenv run will load .env automatically
+"""
+
+from dotenv import load_dotenv; load_dotenv() # Load .env automatically for this code - this may be done already if run by pipenv run
+
 import os
 env_key='SOME_VAR1';    v=os.environ.get(env_key);      print(v)
 env_key='SOME_VAR22';   v=os.environ.get(env_key);      print(v)
